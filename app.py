567bbe568ed8069fb64208acb3b3ce0fb157ffa5
ssinha98 <ssinha20@cmc.edu>
2025-01-26 18:45:41 -0500

    starter script

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/test', methods=['GET'])
+def test_route():
+    variable = request.args.get('variable')
+    return f'variable received: {variable}'
+
+
+if __name__ == '__main__':
+    # Change to Flase or just remove when you deploy
+    app.run(debug=True)
